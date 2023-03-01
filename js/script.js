d6c55e8ec3b6ccae6745b5665d79ad229a09e987
AeroreHighwind <gaston52343@gmail.com>
2023-02-28 23:42:38 -0300

    menu button testing

--- js/script.js
+++ js/script.js
@@ -53,3 +53,17 @@ document.getElementById("cards").onmousemove = e => {
       card.style.setProperty("--mouse-y", `${y}px`);
     };
   }
+
+  //Mobile menu button
+  
+const menuBtn = document.querySelector('#menu-icon');
+const navBar = document.querySelector('.navBar');
+
+menuBtn.addEventListener('click', () =>{
+    if(navBar.style.display == 'none'){
+        navBar.style.display = 'block';
+    }
+    else{
+        navBar.style.display='none';
+    }
+});
